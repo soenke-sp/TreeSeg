16d569240ded4765dd986b9e7608e971635947a6
Sönke Sp <81372622+soenke-sp@users.noreply.github.com>
2023-12-16 14:29:52 +0100

    Update faster_rcnn.py

--- models/faster_rcnn.py
+++ models/faster_rcnn.py
@@ -1,25 +1,73 @@
-import torch, detectron2
-import os
-import sys
-import numpy as np
-import json, cv2, random, statistics
-import matplotlib.pyplot as plt
-import pandas as pd
+# Import libaries:
 import torch
+import detectron2
+import cv2
 import os
+import numpy as np
 
+# Import logger setup function from Detectron2:
 from detectron2.utils.logger import setup_logger
-setup_logger()
-from detectron2.data.datasets import register_coco_instances
+
+# Import relevant modules from Detectron2:
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog, DatasetCatalog
-from detectron2.config import CfgNode as CN
 
-from detectron2.engine import DefaultTrainer
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.data import build_detection_test_loader
+# Set up Detectron2 logger:
+setup_logger()
+cfg = get_cfg()
 
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+# Select Faster-RCNN model:
+model_type = "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+cfg.merge_from_file(model_zoo.get_config_file(model_type))
+
+# Configure the hyperparameter:
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+cfg.MODEL.MASK_ON = True
+cfg.SOLVER.MAX_ITER = 10000
+cfg.SOLVER.IMS_PER_BATCH = 8
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512
+cfg.SOLVER.BASE_LR = 0.001
+cfg.SOLVER.GAMMA = 0.05
+cfg.SOLVER.WEIGHT_DECAY = 1e-4
+cfg.SOLVER.NESTEROV = False
+cfg.SOLVER.MOMENTUM = 0.9
+cfg.SOLVER.WEIGHT_DECAY_NORM = 1e-3
+cfg.SOLVER.BIAS_LR_FACTOR = 2
+cfg.SOLVER.WEIGHT_DECAY_BIAS = 1e-3
+cfg.SOLVER.NUM_DECAYS = 3
+cfg.SOLVER.WARMUP_FACTOR = 1e-3
+cfg.SOLVER.WARMUP_METHOD = "linear"
+
+# Load pre-trained weights for the model:
+cfg.MODEL.WEIGHTS = "faster_rcnn_cp.pth"
+
+# Set the threshold for the prediction:
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+
+# Create predictor:
+predictor = DefaultPredictor(cfg)
+
+# Perform the prediction on images:
+for image_in in os.listdir("input_images/"):
+    
+    # Load image and perform prediction:
+    pred_img = cv2.imread("input_images/"+ image_in)
+    outputs = predictor(pred_img)
+
+    # Prepare predicted masks:
+    masks = outputs["instances"].pred_masks.cpu().numpy()
+    predict = np.zeros((np.shape(masks)[1],np.shape(masks)[2]))
+    
+    # Delete too big masks:
+    i= 0
+    while i != np.shape(masks)[0]:
+        if np.count_nonzero(masks[i]) <= 150000:
+            predict = np.where(predict != 0, 1,masks[i])
+        i += 1
+    
+    # Generate images from masks:
+    image_array = np.where(predict == 1, 255, predict)
+    image_array = np.stack([image_array, image_array, image_array], axis=0).astype(np.uint8)
+    image = cv2.cvtColor(image_array.transpose(1, 2, 0), cv2.COLOR_RGB2BGR)
+    cv2.imwrite("output_images/" + image_in[:-4] + '_mask.jpg', image)  
